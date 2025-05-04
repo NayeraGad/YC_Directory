f4759aa8ff8a7e8f8a17d97faec0d3dabea0825d
NayeraGad <naiera.gad30@gmail.com>
2025-05-04 20:49:13 +0300

    Dynamic views

--- components/View.tsx
+++ components/View.tsx
@@ -3,6 +3,8 @@ import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { Startups_Views_Query } from "@/sanity/lib/queries";
 import { formateNumber } from "@/lib/utils";
+import { writeClient } from "@/sanity/lib/write_client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views } = await client
@@ -10,6 +12,14 @@ const View = async ({ id }: { id: string }) => {
       useCdn: false,
     })
     .fetch(Startups_Views_Query, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: views + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
